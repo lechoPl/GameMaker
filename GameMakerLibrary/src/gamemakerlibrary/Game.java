6bf9b65c583c17c1c26aae79104918c0095d5fa1
Pawel <pawel.krzywodac@gmail.com>
2014-04-01 18:53:44 +0200

    Dodane pole gameName, gettery i settery oraz konstruktor

--- GameMakerLibrary/src/gamemakerlibrary/Game.java
+++ GameMakerLibrary/src/gamemakerlibrary/Game.java
@@ -6,7 +6,12 @@ public class Game implements Serializable {
     public static final String FILE_EXTENSION = "gmf";
     public static final String FILE_EXTENSION_DOT = ".gmf";
 
+    private String gameName;
     private Background background;
+    
+    public Game(String gameName) {
+        this.gameName = gameName;
+    }
 
     public Background getBackground() {
         return this.background;
@@ -14,5 +19,13 @@ public class Game implements Serializable {
 
     public void setBackground(Background background) {
         this.background = background;
+    }
+    
+    public void setName(String name) {
+        this.gameName = name;
+    }
+    
+    public String getName() {
+        return gameName;
     }
 }
