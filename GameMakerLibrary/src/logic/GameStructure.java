09c5b1ed967f5eefa1e37cefe662b04316c83b56
kvasnyk <kamilkwasny92@gmail.com>
2014-05-13 20:03:02 +0200

    Game properties

--- GameMakerLibrary/src/logic/GameStructure.java
+++ GameMakerLibrary/src/logic/GameStructure.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package logic;
 
 import java.awt.Dimension;
@@ -13,19 +7,16 @@ import java.util.LinkedList;
 import resources.GameResources;
 import view.IViewable;
 
-/**
- *
- * @author Pawel
- */
 public class GameStructure implements IViewable, Serializable {
+
     public static final String GAME_PATH = "game.g";
-    
+
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
-    
+
     // fields
     private String gameName;
-    
+
     protected Level currentLevel = null;
     private LinkedList<Level> levels;
 
@@ -33,15 +24,15 @@ public class GameStructure implements IViewable, Serializable {
     public GameStructure() {
         init("Untitled Game");
     }
-    
+
     public GameStructure(String gameName) {
         init(gameName);
     }
-    
+
     private void init(String gameName) {
         this.gameName = gameName;
         this.levels = new LinkedList();
-        
+
         levels.add(new Level("Level1"));
         levels.add(new Level("Level2"));
     }
@@ -49,30 +40,35 @@ public class GameStructure implements IViewable, Serializable {
     public LinkedList<Level> getLevels() {
         return levels;
     }
-    
+
     public void setCurrentLevel(Level lvl) {
         currentLevel = lvl;
     }
-    
+
     public Level getCurrentLevel() {
         return currentLevel;
     }
-    
+
     public void addNewLevel(Level lvl) {
         levels.add(lvl);
     }
-    
+
     public Dimension getWindowSize() {
         return new Dimension(WIDTH, HEIGHT);
     }
-    
+
     public String getName() {
         return this.gameName;
     }
     
+    public void setName(String name) {
+        this.gameName = name;
+    }
+
     @Override
     public void render(Graphics g, GameResources gameResources) {
-        if(currentLevel != null)
+        if (currentLevel != null) {
             currentLevel.render(g, gameResources);
+        }
     }
 }
--- GameMakerLibrary/src/view/GameFrame.java
+++ GameMakerLibrary/src/view/GameFrame.java
@@ -6,6 +6,7 @@
 
 package view;
 
+import javax.swing.JPanel;
 import logic.Game;
 
 /**
